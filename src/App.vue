e52c4c278d9276ae2387b0ed9d5ee4f017c01666
TareqImam <tareqimam800@gmail.com>
2024-09-02 13:18:48 +0600

    cleaned up composition pattern

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
-<script>
+<script setup>
 import { ref } from 'vue';
 
-export default {
-    setup() {
-        const name = ref('Imam');
-        const status = ref('active');
-        const tasks = ref(['One', 'Two', 'Three']);
+const name = ref('Imam');
+const status = ref('active');
+const tasks = ref(['One', 'Two', 'Three']);
 
-        const toggleStatus = () => {
-            if (status.value === 'active') {
-                status.value = 'pending';
-            } else if (status.value === 'pending') {
-                status.value = 'inactive';
-            } else {
-                status.value = 'active';
-            }
-        };
-
-        return {
-            name,
-            status,
-            tasks,
-            toggleStatus,
-        };
-    },
+const toggleStatus = () => {
+    if (status.value === 'active') {
+        status.value = 'pending';
+    } else if (status.value === 'pending') {
+        status.value = 'inactive';
+    } else {
+        status.value = 'active';
+    }
 };
 </script>
 
