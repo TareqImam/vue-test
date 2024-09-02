3db1df10d3336929c6908e549702e5bab06f91ee
TareqImam <tareqimam800@gmail.com>
2024-09-02 18:33:34 +0600

    navbar, hero area, cards, jobs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
 <template>
-    <h1 class="text-2xl">Vue Jobs</h1>
+    <Navbar />
+    <Hero />
+    <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
+    <HomeCards />
+    <JobListings />
 </template>
