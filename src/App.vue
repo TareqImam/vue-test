fe25aa675112c9956927133b05aca605fa0a691c
TareqImam <tareqimam800@gmail.com>
2024-09-03 14:39:27 +0600

    router, router link, active link

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobListings from '@/components/JobListings.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
     <Navbar />
-    <Hero />
-    <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
-    <HomeCards />
-    <JobListings :limit="3" :showButton="true" />
+    <RouterView />
 </template>
